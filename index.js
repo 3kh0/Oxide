cdb549798c502de842d87ee543d6c0b6f79bca53
Echo <echo-the-coder@tuta.io>
2024-07-10 12:07:01 -0400

    Update

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ import { ChemicalServer } from "chemicaljs";
 import express from "express";
 
 const chemical = new ChemicalServer({
-    uv: true,
-    scramjet: false,
-    rammerhead: false
+  uv: true,
+  scramjet: false,
+  rammerhead: false,
 });
 const port = process.env.PORT || 8080;
 
-chemical.app.use(express.static("public", {
+chemical.app.use(
+  express.static("public", {
     index: "index.html",
-    extensions: ["html"]
-}));
+    extensions: ["html"],
+  })
+);
 
 chemical.app.use((req, res) => {
-    res.status(404);
-    res.sendFile("public/404.html", { root: "." });
+  res.status(404);
+  res.sendFile("public/404.html", { root: "." });
 });
 
 chemical.server.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
--- public/main.js
+++ public/main.js
@@ -4,13 +4,13 @@ const searchEngine = document.getElementById("uv-search-engine");
 const error = document.getElementById("uv-error");
 const errorCode = document.getElementById("uv-error-code");
 form.addEventListener("submit", async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    if (window.chemicalLoaded) {
-        const url = search(address.value, searchEngine.value);
+  if (window.chemicalLoaded) {
+    const url = search(address.value, searchEngine.value);
 
-        let frame = document.getElementById("uv-frame");
-        frame.style.display = "block";
-        frame.src = await window.chemicalEncode(url);
-    }
+    let frame = document.getElementById("uv-frame");
+    frame.style.display = "block";
+    frame.src = await window.chemicalEncode(url);
+  }
 });
