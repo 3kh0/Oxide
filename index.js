526881a4d419fedcdb1cbd05609505f7b14c2f84
Echo <echo-the-coder@tuta.io>
2024-07-31 11:43:55 -0400

    Formatting

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ import express from "express";
 const chemical = new ChemicalServer();
 const port = process.env.PORT || 8080;
 
-chemical.use(express.static("public", {
+chemical.use(
+  express.static("public", {
     index: "index.html",
-    extensions: ["html"]
-}));
+    extensions: ["html"],
+  })
+);
 
 chemical.error((req, res) => {
-    res.status(404);
-    res.send("404 Error");
+  res.status(404);
+  res.send("404 Error");
 });
 
 chemical.listen(port, () => {
-    console.log(`Oxide listening on port ${port}`);
+  console.log(`Oxide listening on port ${port}`);
 });
